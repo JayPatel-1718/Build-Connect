118126fbcd71ee659e5209b0e3cd89f3fbae10c2
JayPatel-1718 <jayp704874@gmail.com>
2025-10-04 21:51:34 +0530

    Fixed sample Java program and tested locally

---
+++ src/main/java/com/buildconnect/Main.java
@@ -0,0 +1,7 @@
+package com.buildconnect;  // Must match the folder path: com/buildconnect
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! This is our Build-Connect Java Project.");
+    }
+}
